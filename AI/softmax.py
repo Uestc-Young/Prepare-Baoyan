a6d4424ae9e210fd60189d504bf17eb5f55681ea
Uestc-Young <2126015288@qq.com>
2024-06-30 21:39:22 +0800

    softmax

--- AI/softmax.py
+++ AI/softmax.py
@@ -1,5 +1,31 @@
-'''
-Math function: softmax
+import numpy as np
+import torch
+from typing import List
 
-Softmax(x) = exp(x) / sum(exp(x))
-'''
+def softmax(x: List):
+    '''
+    Math function: softmax
+    Softmax(x) = exp(x) / sum(exp(x))
+
+    在pytorch库的实现中, 为了数值稳定性, 会对x减去最大值, 使得exp(x)不会溢出
+    '''
+    list_max = max(x)
+
+    exp_list = [np.exp(ele-list_max) for ele in x]
+    
+    sum_list = sum(exp_list)
+
+    return_list = [ele/sum_list for ele in exp_list]
+
+    return return_list
+
+# Test
+x = [1,2,3,4,5]
+
+my_softmax = softmax(x)
+
+x_tensor = torch.tensor(x, dtype=torch.float32)
+torch_softmax = torch.softmax(x_tensor, dim=0).numpy()
+
+print("My softmax:", my_softmax)
+print("Torch softmax:", torch_softmax)
